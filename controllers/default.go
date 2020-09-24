6fb298b8a1f074006360e87dd49c0d9236ea4910
IT11717 <2714257837@qq.com>
2020-09-24 23:08:05 +0800

    josn解析1

--- controllers/default.go
+++ controllers/default.go
@@ -1,15 +1,62 @@
 package controllers
 
 import (
+	"BeegoProject001/models"
+	"encoding/json"
+	"fmt"
 	"github.com/astaxie/beego"
+	"io/ioutil"
 )
 
 type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
-	c.Data["Website"] = "www.baidu.com"
-	c.Data["Email"] = "2714257837@qq.com"
-	c.TplName = "index.tpl"
+//func (c *MainController) Get() {
+//	uersName := c.Ctx.Input.Query("use")
+//	passWord := c.Ctx.Input.Query("psd")
+//	if uersName != "admin" || passWord != "123456" {
+//
+//		c.Ctx.WriteString("信息错误")
+//
+//	} else {
+//		c.Ctx.WriteString("成功输入")
+//	}
+//	c.Data["Website"] = "www.baidu.com"
+//	c.Data["Email"] = "2714257837@qq.com"
+//	c.TplName = "index.tpl"
+//}
+//func (c *MainController) Post() {
+//	//接受post请求数据
+//	name := c.Ctx.Request.FormValue("name")
+//	age := c.Ctx.Request.FormValue("age")
+//	sex := c.Ctx.Request.FormValue("sex")
+//	fmt.Println(name, age, sex)
+//	if name != "wangrenzhi" && age != "18" && sex != "maie" {
+//		c.Ctx.WriteString("数据验证失败")
+//		return
+//
+//	}
+//	c.Ctx.WriteString("验证数据成功")
+//
+//}
+func (c *MainController) Post() {
+	//解析前端提交的json格式的数据
+	var person models.Preson
+	dataBytes, err := ioutil.ReadAll(c.Ctx.Request.Body)
+	if err != nil {
+		c.Ctx.WriteString("数据接受错误，请重试")
+		return
+
+	}
+	err = json.Unmarshal(dataBytes, &person)
+	if err != nil {
+		c.Ctx.WriteString("数据解析失败，请重试")
+		return
+	}
+	fmt.Println("姓名", person.Name)
+	fmt.Println("年龄", person.Age)
+	fmt.Println("性别", person.Sex)
+	c.Ctx.WriteString("数据解析成功")
+
 }
---
+++ models/person.go
@@ -0,0 +1,7 @@
+package models
+
+type Preson struct {
+	Name  string
+	Age   int
+	Sex  string
+}
